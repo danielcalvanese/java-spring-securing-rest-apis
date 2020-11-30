cc06da2227711870910622af9828443ab462da76
Daniel Calvanese <Daniel.Calvanese@Gmail.com>
2020-11-30 04:25:10 -0500

    Added module 4 tasks.

--- src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
+++ src/main/java/io/jzheaux/springsecurity/resolutions/ResolutionsApplication.java
@@ -2,38 +2,50 @@ package io.jzheaux.springsecurity.resolutions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import static org.springframework.http.HttpMethod.GET;
 
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class ResolutionsApplication extends WebSecurityConfigurerAdapter {
-	public static void main(String[] args) {
-		SpringApplication.run(ResolutionsApplication.class, args);
-	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		/*
-		http.authorizeRequests(authz ->
-			authz.mvcMatchers(GET, "/resolutions", "/resolution/**").hasAuthority("resolution:read")
-			.anyRequest().hasAuthority("resolution:write")
-		).httpBasic(basic -> {});
-		 */
-		// Using preauthorize we transformed the above code into the below code.
-		http.authorizeRequests(authz ->
-			authz.anyRequest().authenticated()
-		).httpBasic(basic -> {});
+		http
+				.authorizeRequests(authz -> authz
+						.mvcMatchers(GET, "/resolutions", "/resolution/**").hasAuthority("resolution:read")
+						.anyRequest().hasAuthority("resolution:write"))
+				.httpBasic(basic -> {})
+				.cors(cors -> {});
 	}
 
 	@Bean
 	public UserDetailsService userDetailsService(UserRepository users) {
 		return new UserRepositoryUserDetailsService(users);
 	}
+
+	@Bean
+	public WebMvcConfigurer webMvcConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("http://localhost:4000")
+						.allowedMethods("HEAD")
+						.allowedHeaders("Authorization");
+			}
+		};
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResolutionsApplication.class, args);
+	}
+
 }
